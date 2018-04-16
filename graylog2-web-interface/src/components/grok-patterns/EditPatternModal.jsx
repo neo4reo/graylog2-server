1240b9ffe00c87226fee78b2011b75a4ff9c0302
Konrad Merz <konrad@graylog.com>
2018-04-16 16:39:14 +0200

    Make use of new test endpoint
    - Add a sample data text area and a test result area
    - Add a button which takes the name, pattern and sample data to send it to the server
    - Show result in test result area
    - Add new snapshot tests
    - Add help text to make it easier for the user

--- graylog2-web-interface/src/components/grok-patterns/EditPatternModal.jsx
+++ graylog2-web-interface/src/components/grok-patterns/EditPatternModal.jsx
@@ -2,26 +2,41 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { BootstrapModalForm, Input } from 'components/bootstrap';
 
+import { Button } from 'react-bootstrap';
+
 class EditPatternModal extends React.Component {
   static propTypes = {
     id: PropTypes.string,
     name: PropTypes.string,
     pattern: PropTypes.string,
     create: PropTypes.bool,
+    sample_data: PropTypes.string,
     savePattern: PropTypes.func.isRequired,
+    testPattern: PropTypes.func,
     validPatternName: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    id: '',
+    name: '',
+    pattern: '',
+    create: false,
+    sample_data: '',
+    testPattern: () => {},
   };
 
   state = {
     id: this.props.id,
     name: this.props.name,
     pattern: this.props.pattern,
+    sample_data: this.props.sample_data,
+    test_result: '',
     error: false,
     error_message: '',
   };
 
   openModal = () => {
-    this.refs.modal.open();
+    this.modal.open();
   };
 
   _onPatternChange = (event) => {
@@ -38,18 +53,22 @@ class EditPatternModal extends React.Component {
     }
   };
 
+  _onSampleDataChange = (event) => {
+    this.setState({sample_data: event.target.value});
+  };
+
   _getId = (prefixIdName) => {
     return this.state.name !== undefined ? prefixIdName + this.state.name : prefixIdName;
   };
 
   _closeModal = () => {
-    this.refs.modal.close();
+    this.modal.close();
   };
 
   _saved = () => {
     this._closeModal();
     if (this.props.create) {
-      this.setState({ name: '', pattern: '' });
+      this.setState({ name: '', pattern: '', sample_data: '', test_result: '' });
     }
   };
 
@@ -59,6 +78,12 @@ class EditPatternModal extends React.Component {
     if (!pattern.error) {
       this.props.savePattern(pattern, this._saved);
     }
+  };
+
+  _testPattern = () => {
+    this.props.testPattern(this.state, (response) => {
+      this.setState({ test_result: JSON.stringify(response, null, 2) });
+    });
   };
 
   render() {
@@ -73,26 +98,43 @@ class EditPatternModal extends React.Component {
         <button onClick={this.openModal} className={this.props.create ? 'btn btn-success' : 'btn btn-info btn-xs'}>
           {triggerButtonContent}
         </button>
-        <BootstrapModalForm ref="modal"
-                                    title={`${this.props.create ? 'Create' : 'Edit'} Grok Pattern ${this.state.name}`}
-                                    onSubmitForm={this._save}
-                                    submitButtonText="Save">
+        <BootstrapModalForm ref={(node) => { this.modal = node; }}
+                            title={`${this.props.create ? 'Create' : 'Edit'} Grok Pattern ${this.state.name}`}
+                            onSubmitForm={this._save}
+                            submitButtonText="Save">
           <fieldset>
             <Input type="text"
-                           id={this._getId('pattern-name')}
-                           label="Name"
-                           onChange={this._onNameChange}
-                           value={this.state.name}
-                           bsStyle={this.state.error ? 'error' : null}
-                           help={this.state.error ? this.state.error_message : null}
-                           autoFocus
-                           required />
+                   id={this._getId('pattern-name')}
+                   label="Name"
+                   onChange={this._onNameChange}
+                   value={this.state.name}
+                   bsStyle={this.state.error ? 'error' : null}
+                   help={this.state.error ? this.state.error_message : "Under this name the pattern will be stored and can be used like: '%{THISNAME}' later on "}
+                   autoFocus
+                   required />
             <Input type="textarea"
-                           id={this._getId('pattern')}
-                           label="Pattern"
-                           onChange={this._onPatternChange}
-                           value={this.state.pattern}
-                           required />
+                   id={this._getId('pattern')}
+                   label="Pattern"
+                   help="The pattern which will match the log line e.g: '%{IP:client}' or '.*?'"
+                   onChange={this._onPatternChange}
+                   value={this.state.pattern}
+                   required />
+            <Input type="textarea"
+                   id={this._getId('sample_data')}
+                   label="Sample Data"
+                   help="Here you can add sample data to test your pattern"
+                   onChange={this._onSampleDataChange}
+                   value={this.state.sample_data} />
+            <Button bsStyle="info" onClick={this._testPattern}>Test with Sample Data</Button>
+            <br />
+            <br />
+            <Input type="textarea"
+                   id={this._getId('test_result')}
+                   readOnly
+                   rows={8}
+                   help="Will contain the result of your test in a JSON format"
+                   label="Test Result"
+                   value={this.state.test_result} />
           </fieldset>
         </BootstrapModalForm>
       </span>
